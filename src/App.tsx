965ab15d547e9c830421c698a36a17b0e052d4f3
JWW127 <19317273+JWW127@users.noreply.github.com>
2023-02-15 22:42:27 -0600

    basically got boiler plate working, and some simple animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import './App.css'
 import { gsap } from 'gsap'
+import { useLayoutEffect, useRef } from 'react'
 
 function App() {
+    //using this to target element
+    const root = useRef(null);
 
-  return (
-    <div className="App">
-<h1>GSAP</h1>
-    </div>
-  )
+    // this is what is calling at load
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.to(".one", { rotation: 360 })
+            gsap.to(".two", { rotation: 180 })
+            gsap.timeline().to(".three", { x: -50, y: -50}).to(".three", {x: 0, y: 0})
+        }, root)
+
+        return () => ctx.revert()
+    }, [])
+    return (
+        <div ref={root} className="app">
+            <div className="one">ONE</div>
+            <div className="two">TWO</div>
+            <div className="three">THREE</div>
+        </div>
+    )
 }
 
 export default App
